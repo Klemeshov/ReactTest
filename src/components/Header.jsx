66acc2b3818d023c9280247943d1166cbfce801c
Dmitry Klemeshov <dima-0510@mail.ru>
2020-02-29 16:41:33 +0300

    что-то есть

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () =>{
+    return(
+        <header className='header'>
+            <div className='headerContent'>
+                <img src={"img/Header/logo.png"} alt={""}/>
+                <h1>content</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
