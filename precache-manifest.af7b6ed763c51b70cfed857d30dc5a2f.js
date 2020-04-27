216329425bfec022018337416a4476e9bee2afff
Dmitry Klemeshov <dima-0510@mail.ru>
2020-04-28 00:35:02 +0300

    Updates

---
+++ precache-manifest.af7b6ed763c51b70cfed857d30dc5a2f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7782ba9f57074e6495c717c42f3cf0",
+    "url": "/ReactTest/index.html"
+  },
+  {
+    "revision": "22f37e833d74bc4d2754",
+    "url": "/ReactTest/static/css/4.bfe6ef25.chunk.css"
+  },
+  {
+    "revision": "2f498fd626bfcb7c7e6d",
+    "url": "/ReactTest/static/css/5.da92f907.chunk.css"
+  },
+  {
+    "revision": "e834f48a987b5fd4daf2",
+    "url": "/ReactTest/static/css/main.81811b32.chunk.css"
+  },
+  {
+    "revision": "e82f1ed70daf0798a466",
+    "url": "/ReactTest/static/js/2.f8827001.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ReactTest/static/js/2.f8827001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eeaba0914f364748c82",
+    "url": "/ReactTest/static/js/3.62282cfb.chunk.js"
+  },
+  {
+    "revision": "22f37e833d74bc4d2754",
+    "url": "/ReactTest/static/js/4.7c6be50e.chunk.js"
+  },
+  {
+    "revision": "2f498fd626bfcb7c7e6d",
+    "url": "/ReactTest/static/js/5.e51f6728.chunk.js"
+  },
+  {
+    "revision": "7ea9fcd35af335b03719",
+    "url": "/ReactTest/static/js/6.010a7747.chunk.js"
+  },
+  {
+    "revision": "e834f48a987b5fd4daf2",
+    "url": "/ReactTest/static/js/main.c3633410.chunk.js"
+  },
+  {
+    "revision": "7c4ddacd7a181c4189a6",
+    "url": "/ReactTest/static/js/runtime-main.af0f86f4.js"
+  },
+  {
+    "revision": "a2e8adc641a6e6b8ec5e5b59c25a635d",
+    "url": "/ReactTest/static/media/Loader.a2e8adc6.svg"
+  },
+  {
+    "revision": "86e9d33ed7c96104f7f4e417b977963b",
+    "url": "/ReactTest/static/media/Messages.86e9d33e.png"
+  },
+  {
+    "revision": "779b381d52e2c52279d8b99def346d7d",
+    "url": "/ReactTest/static/media/Music.779b381d.png"
+  },
+  {
+    "revision": "672cc29e3e2faafa697977d03eae9e76",
+    "url": "/ReactTest/static/media/Profile.672cc29e.png"
+  },
+  {
+    "revision": "4cf9126f74ae73cc72cd7b2ec3794572",
+    "url": "/ReactTest/static/media/Setting.4cf9126f.png"
+  },
+  {
+    "revision": "8204bf43eaec04006d1199116feb8852",
+    "url": "/ReactTest/static/media/Users.8204bf43.png"
+  },
+  {
+    "revision": "1aaa1e93b992cffa068fbd843eed4282",
+    "url": "/ReactTest/static/media/avatar.1aaa1e93.jpg"
+  }
+]);
