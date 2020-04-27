72644da9f68a296293c7b8436969eff7f216bf9e
Dmitry Klemeshov <dima-0510@mail.ru>
2020-04-28 01:39:35 +0300

    add ssl on server

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import * as axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://192.168.0.105:5000',
     withCredentials: true
 });
 
